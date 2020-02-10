b6af724f67d0f7539265e57038dc727534872070
parsons <pycaijjhou@hotmail.com>
2020-02-10 01:30:11 -0800

    Parsons has no life

---
+++ ASCT_Razor_Page/Pages/AdminUI.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ASCT_Razor_Page.Pages
+{
+    public class AdminUIModel : PageModel
+    {
+        private readonly ILogger<AdminUIModel> _logger;
+
+        public AdminUIModel(ILogger<AdminUIModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
