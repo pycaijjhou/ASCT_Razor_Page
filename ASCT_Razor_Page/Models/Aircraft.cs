5fd12807757db9b2d09881aa5c1008d06aa7166a
parsons <pycaijjhou@hotmail.com>
2020-02-11 15:41:00 -0800

    Aircraft model

---
+++ ASCT_Razor_Page/Models/Aircraft.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ASCT_Razor_Page.Models
+{
+    public class Aircraft
+    {
+        public string Tail_Number { get; set; }
+
+        public string LRU_Name { get; set; }
+
+        public string LRU_Part_Number { get; set; }
+
+        public string Software_Name { get; set; }
+
+        public string Software_Part_Number { get; set; }
+
+        public string EC_Number { get; set; }
+
+        public string Pre_Post_Condition { get; set; }
+
+        public Aircraft()
+        {
+        }
+    }
+}
