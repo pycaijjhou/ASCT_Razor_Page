194faf390d2a3aab5e1c1bbe395b349a974f5e0c
Junhong Cai <pycaijjhou@hotmail.com>
2020-02-24 03:57:47 -0800

    scaffolding

---
+++ ASCT_Razor_Page/Model/Aircrafts.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASCT_Razor_Page.Model
+{
+    public partial class Aircrafts
+    {
+        [Column("TAIL_NUM")]
+        [StringLength(50)]
+        public string TailNum { get; set; }
+        [Column("LRU_NAME")]
+        [StringLength(50)]
+        public string LruName { get; set; }
+        [Column("LRU_PN")]
+        [StringLength(50)]
+        public string LruPn { get; set; }
+        [Column("SOFTWARE_NAME")]
+        [StringLength(50)]
+        public string SoftwareName { get; set; }
+        [Column("SOFTWARE_PN")]
+        [StringLength(50)]
+        public string SoftwarePn { get; set; }
+        [Column("FLEET_TYPE")]
+        [StringLength(50)]
+        public string FleetType { get; set; }
+        [Column("EC_NUM")]
+        [StringLength(50)]
+        public string EcNum { get; set; }
+        [Column("PRE")]
+        [StringLength(50)]
+        public string Pre { get; set; }
+        [Column("POST")]
+        [StringLength(50)]
+        public string Post { get; set; }
+        [Column("CURR_STATE")]
+        [StringLength(50)]
+        public string CurrState { get; set; }
+    }
+}
---
+++ ASCT_Razor_Page/Model/AircraftsContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ASCT_Razor_Page.Model
+{
+    public partial class AircraftsContext : DbContext
+    {
+        public AircraftsContext()
+        {
+        }
+
+        public AircraftsContext(DbContextOptions<AircraftsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Aircrafts> Aircrafts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=asctserver.database.windows.net;Initial Catalog=ASCT;User ID=ASCTadmin;Password=Admin1234");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aircrafts>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.CurrState).IsUnicode(false);
+
+                entity.Property(e => e.EcNum).IsUnicode(false);
+
+                entity.Property(e => e.FleetType).IsUnicode(false);
+
+                entity.Property(e => e.LruName).IsUnicode(false);
+
+                entity.Property(e => e.LruPn).IsUnicode(false);
+
+                entity.Property(e => e.Post).IsUnicode(false);
+
+                entity.Property(e => e.Pre).IsUnicode(false);
+
+                entity.Property(e => e.SoftwareName).IsUnicode(false);
+
+                entity.Property(e => e.SoftwarePn).IsUnicode(false);
+
+                entity.Property(e => e.TailNum).IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
